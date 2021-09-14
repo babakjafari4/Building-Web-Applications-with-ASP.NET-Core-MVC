627f0b18b8253b4d8c12e8b911efa818cf9413dd
Babak Jafari <ba.jafari@deltaban.local>
2021-09-14 18:03:22 +0430

    PieController has added to the project.
    Includes:
    On branch dev
    Changes to be committed:
    	new file:   src/Presentation/MVCAppPractice.Presentation.MVC/Controllers/PieController.cs

---
+++ src/Presentation/MVCAppPractice.Presentation.MVC/Controllers/PieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAppPractice.Presentation.MVC.Controllers
+{
+    public class PieController:Controller
+    {
+        public PieController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return Ok("Hello from pie controller. this is another test.");
+        }
+    }
+}
